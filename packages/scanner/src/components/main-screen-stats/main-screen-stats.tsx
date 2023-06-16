bc42bfdf5636df6a87f53267581b436a579604b5
Arthur <stfriman13@gmail.com>
2023-06-16 13:30:01 +0300

    scanner - main-scrren-stats amounts fix

--- packages/scanner/src/components/main-screen-stats/main-screen-stats.tsx
+++ packages/scanner/src/components/main-screen-stats/main-screen-stats.tsx
@@ -1,37 +1,51 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useLiveQuery } from 'dexie-react-hooks';
 import dayjs from 'dayjs';
 
 import { AppContext } from '~/app-context';
+import type { Transaction } from '~/db';
 import { db, getVolsOnField } from '~/db';
 import { getToday } from '~/lib/date';
 
 import style from './main-screen-stats.module.css';
 
 export const MainScreenStats = () => {
+    const [volsFedAmount, setVolsFedAmount] = useState(0);
     const { mealTime } = useContext(AppContext);
 
     const volsOnField = useLiveQuery(async () => (await getVolsOnField(getToday())).length, [mealTime], 0);
 
-    const volsFed = useLiveQuery(
+    const todayTxs = useLiveQuery(
         async () => {
             const today = getToday();
             return db.transactions
                 .where('ts')
                 .between(dayjs(today).add(7, 'h').unix(), dayjs(today).add(31, 'h').unix())
-                .filter((tx) => {
-                    return tx.mealTime === mealTime && tx.amount !== 0;
-                })
-                .count();
+                .toArray();
         },
         [mealTime],
-        0
-    );
+        []
+    ) as Array<Transaction>;
+
+    useEffect(() => {
+        setVolsFedAmount(() => {
+            if (todayTxs.length > 0) {
+                return todayTxs.reduce((acc, curr) => {
+                    if (curr.mealTime === mealTime) {
+                        return acc + curr.amount;
+                    }
+                    return acc;
+                }, 0);
+            }
+            return 0;
+        });
+    }, [mealTime, todayTxs]);
+
     return (
         <div className={style.mainScreenStats}>
             <span>На поле: {volsOnField}</span>
-            <span>Покормлено: {volsFed}</span>
-            <span>Осталось: {volsOnField > volsFed ? volsOnField - volsFed : 0}</span>
+            <span>Покормлено: {volsFedAmount}</span>
+            <span>Осталось: {volsOnField > volsFedAmount ? volsOnField - volsFedAmount : 0}</span>
         </div>
     );
 };
