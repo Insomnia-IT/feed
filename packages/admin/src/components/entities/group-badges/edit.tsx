7d2e5cc6df77a0214bdb369c83cb20ba8b74802a
Artem Novikov <novartole@gmail.com>
2023-07-03 20:06:35 +0000

    fix[admin, 485d07b2]: Run lint-fix:js.

--- packages/admin/src/components/entities/group-badges/edit.tsx
+++ packages/admin/src/components/entities/group-badges/edit.tsx
@@ -1,22 +1,34 @@
-import { Button, Edit, EditButton, Form, Space, Table, TextField, Typography, useForm, useTable } from '@pankod/refine-antd';
+import {
+    Button,
+    Edit,
+    EditButton,
+    Form,
+    Space,
+    Table,
+    TextField,
+    Typography,
+    useForm,
+    useTable
+} from '@pankod/refine-antd';
 import type { IResourceComponentsProps } from '@pankod/refine-core';
 import { useTranslate, useUpdateMany } from '@pankod/refine-core';
-import { DeleteOutlined } from "@ant-design/icons";
-import { Popconfirm } from "antd";
+import { DeleteOutlined } from '@ant-design/icons';
+import { Popconfirm } from 'antd';
 
 import 'react-mde/lib/styles/css/react-mde-all.css';
+
+import { useEffect, useMemo, useState } from 'react';
 
 import type { GroupBadgeEntity, VolEntity } from '~/interfaces';
 
 import { CreateEdit } from './common';
-import { useMemo, useState, useEffect } from 'react';
 
 const { Title } = Typography;
 
 export const GroupBadgeEdit: FC<IResourceComponentsProps> = () => {
     const translate = useTranslate();
 
-    const [volunteers, setVolunteers] = useState<Array<VolEntity & { markedDeleted: Boolean }>>([]);
+    const [volunteers, setVolunteers] = useState<Array<VolEntity & { markedDeleted: boolean }>>([]);
 
     const { mutate } = useUpdateMany();
 
@@ -26,7 +38,7 @@ export const GroupBadgeEdit: FC<IResourceComponentsProps> = () => {
                 resource: 'volunteers',
                 ids: volunteers.filter((vol) => vol.markedDeleted).map((vol) => vol.id),
                 values: { group_badge: null }
-            })
+            });
         }
     });
 
@@ -48,20 +60,18 @@ export const GroupBadgeEdit: FC<IResourceComponentsProps> = () => {
     });
 
     useEffect(
-        () => setVolunteers(
-            (prevState) => 
-                tableProps.dataSource?.map((vol) => ({ 
-                    ...vol, 
-                    markedDeleted: prevState.find((prevVol) => prevVol.id === vol.id)?.markedDeleted ?? false
-                })) ?? []
-        ),
+        () =>
+            setVolunteers(
+                (prevState) =>
+                    tableProps.dataSource?.map((vol) => ({
+                        ...vol,
+                        markedDeleted: prevState.find((prevVol) => prevVol.id === vol.id)?.markedDeleted ?? false
+                    })) ?? []
+            ),
         [tableProps.dataSource]
     );
 
-    const dataSource = useMemo(
-        () => volunteers.filter((vol) => !vol.markedDeleted),
-        [volunteers]
-    );
+    const dataSource = useMemo(() => volunteers.filter((vol) => !vol.markedDeleted), [volunteers]);
 
     return (
         <Edit saveButtonProps={saveButtonProps}>
@@ -69,59 +79,58 @@ export const GroupBadgeEdit: FC<IResourceComponentsProps> = () => {
                 <CreateEdit />
             </Form>
             <Title level={5}>Волонтеры</Title>
-                <Table {...tableProps} dataSource={dataSource} rowKey='id'>
-                    <Table.Column
-                        dataIndex='nickname'
-                        key='nickname'
-                        title='Позывной'
-                        render={(value) => <TextField value={value} />}
-                    />
-                    <Table.Column
-                        dataIndex='name'
-                        key='name'
-                        title='Имя'
-                        render={(value) => <TextField value={value} />}
-                    />
-                    <Table.Column
-                        dataIndex='lastname'
-                        key='lastname'
-                        title='Фамилия'
-                        render={(value) => <TextField value={value} />}
-                    />
-                    <Table.Column
-                        dataIndex='departments'
-                        key='departments    '
-                        title='Службы'
-                        render={(value) => <TextField value={value.map(({ name }) => name).join(', ')} />}
-                    />
-                    <Table.Column<Pick<VolEntity, 'id'>>
-                        title='Действия'
-                        dataIndex='actions'
-                        render={(_, record) => (
-                            <Space>
-                                <EditButton
-                                    hideText
-                                    size='small'
-                                    resourceNameOrRouteName='volunteers'
-                                    recordItemId={record.id}
-                                />
-                                <Popconfirm
-                                    title={translate("buttons.confirm", "Are you sure?")}
-                                    okText={translate("buttons.delete", "Delete")}
-                                    cancelText={translate("buttons.cancel", "Cancel")}
-                                    okType="danger"
-                                    onConfirm={(): void => {
-                                        const index = volunteers.findIndex((vol) => vol.id === record.id);
-                                        if (index > -1)
-                                            setVolunteers(volunteers.map((vol, i) => i === index ? (vol.markedDeleted = true, vol) : vol));
-                                    }}
-                                >
-                                    <Button icon={<DeleteOutlined />} danger size='small' />
-                                </Popconfirm>
-                            </Space>
-                        )}
-                    />
-                </Table>
+            <Table {...tableProps} dataSource={dataSource} rowKey='id'>
+                <Table.Column
+                    dataIndex='nickname'
+                    key='nickname'
+                    title='Позывной'
+                    render={(value) => <TextField value={value} />}
+                />
+                <Table.Column dataIndex='name' key='name' title='Имя' render={(value) => <TextField value={value} />} />
+                <Table.Column
+                    dataIndex='lastname'
+                    key='lastname'
+                    title='Фамилия'
+                    render={(value) => <TextField value={value} />}
+                />
+                <Table.Column
+                    dataIndex='departments'
+                    key='departments    '
+                    title='Службы'
+                    render={(value) => <TextField value={value.map(({ name }) => name).join(', ')} />}
+                />
+                <Table.Column<Pick<VolEntity, 'id'>>
+                    title='Действия'
+                    dataIndex='actions'
+                    render={(_, record) => (
+                        <Space>
+                            <EditButton
+                                hideText
+                                size='small'
+                                resourceNameOrRouteName='volunteers'
+                                recordItemId={record.id}
+                            />
+                            <Popconfirm
+                                title={translate('buttons.confirm', 'Are you sure?')}
+                                okText={translate('buttons.delete', 'Delete')}
+                                cancelText={translate('buttons.cancel', 'Cancel')}
+                                okType='danger'
+                                onConfirm={(): void => {
+                                    const index = volunteers.findIndex((vol) => vol.id === record.id);
+                                    if (index > -1)
+                                        setVolunteers(
+                                            volunteers.map((vol, i) =>
+                                                i === index ? ((vol.markedDeleted = true), vol) : vol
+                                            )
+                                        );
+                                }}
+                            >
+                                <Button icon={<DeleteOutlined />} danger size='small' />
+                            </Popconfirm>
+                        </Space>
+                    )}
+                />
+            </Table>
         </Edit>
     );
 };
