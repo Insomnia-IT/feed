bd2b4f6c44e36b0c092d8a69c5a4a16edcf9a4c3
Artem Novikov <novartole@gmail.com>
2023-07-03 19:17:24 +0000

    feat[admin; 485d07b2; group badge]: Added delete button for vols.

--- packages/admin/src/components/entities/group-badges/edit.tsx
+++ packages/admin/src/components/entities/group-badges/edit.tsx
@@ -1,16 +1,34 @@
-import { Edit, EditButton, Form, Space, Table, TextField, Typography, useForm, useTable } from '@pankod/refine-antd';
+import { Button, Edit, EditButton, Form, Space, Table, TextField, Typography, useForm, useTable } from '@pankod/refine-antd';
 import type { IResourceComponentsProps } from '@pankod/refine-core';
+import { useTranslate, useUpdateMany } from '@pankod/refine-core';
+import { DeleteOutlined } from "@ant-design/icons";
+import { Popconfirm } from "antd";
 
 import 'react-mde/lib/styles/css/react-mde-all.css';
 
 import type { GroupBadgeEntity, VolEntity } from '~/interfaces';
 
 import { CreateEdit } from './common';
+import { useMemo, useState, useEffect } from 'react';
 
 const { Title } = Typography;
 
 export const GroupBadgeEdit: FC<IResourceComponentsProps> = () => {
-    const { formProps, id, saveButtonProps } = useForm<GroupBadgeEntity>();
+    const translate = useTranslate();
+
+    const [volunteers, setVolunteers] = useState<Array<VolEntity & { markedDeleted: Boolean }>>([]);
+
+    const { mutate } = useUpdateMany();
+
+    const { formProps, id, saveButtonProps } = useForm<GroupBadgeEntity>({
+        onMutationSuccess: () => {
+            mutate({
+                resource: 'volunteers',
+                ids: volunteers.filter((vol) => vol.markedDeleted).map((vol) => vol.id),
+                values: { group_badge: null }
+            })
+        }
+    });
 
     const { tableProps } = useTable<VolEntity>({
         resource: 'volunteers',
@@ -29,13 +47,29 @@ export const GroupBadgeEdit: FC<IResourceComponentsProps> = () => {
         ]
     });
 
+    useEffect(
+        () => setVolunteers(
+            (prevState) => 
+                tableProps.dataSource?.map((vol) => ({ 
+                    ...vol, 
+                    markedDeleted: prevState.find((prevVol) => prevVol.id === vol.id)?.markedDeleted ?? false
+                })) ?? []
+        ),
+        [tableProps.dataSource]
+    );
+
+    const dataSource = useMemo(
+        () => volunteers.filter((vol) => !vol.markedDeleted),
+        [volunteers]
+    );
+
     return (
         <Edit saveButtonProps={saveButtonProps}>
             <Form {...formProps} layout='vertical'>
                 <CreateEdit />
-
-                <Title level={5}>Волонтеры</Title>
-                <Table {...tableProps} rowKey='id'>
+            </Form>
+            <Title level={5}>Волонтеры</Title>
+                <Table {...tableProps} dataSource={dataSource} rowKey='id'>
                     <Table.Column
                         dataIndex='nickname'
                         key='nickname'
@@ -60,7 +94,7 @@ export const GroupBadgeEdit: FC<IResourceComponentsProps> = () => {
                         title='Службы'
                         render={(value) => <TextField value={value.map(({ name }) => name).join(', ')} />}
                     />
-                    <Table.Column<GroupBadgeEntity>
+                    <Table.Column<Pick<VolEntity, 'id'>>
                         title='Действия'
                         dataIndex='actions'
                         render={(_, record) => (
@@ -71,11 +105,23 @@ export const GroupBadgeEdit: FC<IResourceComponentsProps> = () => {
                                     resourceNameOrRouteName='volunteers'
                                     recordItemId={record.id}
                                 />
+                                <Popconfirm
+                                    title={translate("buttons.confirm", "Are you sure?")}
+                                    okText={translate("buttons.delete", "Delete")}
+                                    cancelText={translate("buttons.cancel", "Cancel")}
+                                    okType="danger"
+                                    onConfirm={(): void => {
+                                        const index = volunteers.findIndex((vol) => vol.id === record.id);
+                                        if (index > -1)
+                                            setVolunteers(volunteers.map((vol, i) => i === index ? (vol.markedDeleted = true, vol) : vol));
+                                    }}
+                                >
+                                    <Button icon={<DeleteOutlined />} danger size='small' />
+                                </Popconfirm>
                             </Space>
                         )}
                     />
                 </Table>
-            </Form>
         </Edit>
     );
 };
