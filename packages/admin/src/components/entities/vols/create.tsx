5241d18d6557e4c152117a990ac0fa224b1aaa5e
volnyagin <vconst@users.noreply.github.com>
2023-07-07 23:38:46 +0300

    Показывать предупреждение, если дата активности еще не наступила

--- packages/admin/src/components/entities/vols/create.tsx
+++ packages/admin/src/components/entities/vols/create.tsx
@@ -1,50 +1,26 @@
-import { Create, Form, Input, Modal, Select, useForm, useSelect } from '@pankod/refine-antd';
+import { Create, Form, useForm } from '@pankod/refine-antd';
 import type { IResourceComponentsProps } from '@pankod/refine-core';
-import { useState } from 'react';
 
 import type { VolEntity } from '~/interfaces';
 
 import { CreateEdit } from './common';
+import useSaveConfirm from './useSaveConfirm';
 
 export const VolCreate: FC<IResourceComponentsProps> = () => {
     const { form, formProps, saveButtonProps } = useForm<VolEntity>();
-    const [isSaveConfirmationModalOpened, setIsSaveConfirmationModalOpened] = useState(false);
-
-    const handleOk = () => {
-        setIsSaveConfirmationModalOpened(false);
-        saveButtonProps?.onClick();
-    };
-
-    const handleCancel = () => {
-        setIsSaveConfirmationModalOpened(false);
-    };
+    const { onClick, renderModal } = useSaveConfirm(form, saveButtonProps);
 
     return (
         <Create
             saveButtonProps={{
                 ...saveButtonProps,
-                onClick: () => {
-                    if (!form.getFieldValue('is_active')) {
-                        setIsSaveConfirmationModalOpened(true);
-                    } else {
-                        saveButtonProps?.onClick();
-                    }
-                }
+                onClick
             }}
         >
             <Form {...formProps} layout='vertical'>
                 <CreateEdit form={form} />
             </Form>
-            <Modal
-                title='Сохранение'
-                open={isSaveConfirmationModalOpened}
-                onOk={handleOk}
-                onCancel={handleCancel}
-                okText='Сохранить'
-            >
-                <p>Пользователь не активирован. Вы уверены, что хотите продолжить сохранение?</p>
-                <p>Новый неактивированный пользователь будет заблокирован после синхронизации с ноушен.</p>
-            </Modal>
+            {renderModal()}
         </Create>
     );
 };
