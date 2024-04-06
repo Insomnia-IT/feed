a7e095d733d1824539ffc48624c7d5f24d766265
volnyagin <vconst@users.noreply.github.com>
2024-04-06 19:59:10 +0300

    fix swagger

--- docs/swagger-dist/swagger-initializer.js
+++ docs/swagger-dist/swagger-initializer.js
@@ -5,7 +5,7 @@ window.onload = function () {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: 'swaggers/' + swaggerName + ".yaml",
+    url: '/feed/swaggers/' + swaggerName + ".yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
