ba13b11303a59aeacedecb442b0a0d6bd178e2a2
Arthur <stfriman13@gmail.com>
2024-04-14 16:02:29 +0300

    scanner: add colors, fix postcss.config.js

--- packages/scanner/postcss.config.js
+++ packages/scanner/postcss.config.js
@@ -23,9 +23,9 @@ module.exports = (ctx) => {
             },
             'postcss-modules': ctx.options.modules
                 ? {
-                      getJSON: ctx.extractModules || (() => {}),
-                      generateScopedName: 'ui__[local]___[hash:base64:7]'
-                  }
+                    getJSON: ctx.extractModules || (() => {}),
+                    generateScopedName: 'ui__[local]___[hash:base64:7]'
+                }
                 : false,
             'postcss-mixins': {
                 mixinsFiles: path.resolve(__dirname, 'packages/common/assets/styles/mixins.css')
@@ -33,40 +33,40 @@ module.exports = (ctx) => {
             'postcss-nested': {},
             'postcss-media-minmax': {},
             'postcss-custom-media': {
-                importFrom: path.resolve(__dirname, 'src/common/media.css')
+                importFrom: path.resolve(__dirname, 'src/shared/common/media.css')
             },
             'postcss-custom-properties': {
                 preserve: false,
                 disableDeprecationNotice: true,
                 importFrom: [
-                    path.resolve(__dirname, 'src/common/vars.css'),
-                    path.resolve(__dirname, 'src/common/colors.css')
+                    path.resolve(__dirname, 'src/shared/common/vars.css'),
+                    path.resolve(__dirname, 'src/shared/common/colors.css')
                 ],
                 exportTo: DEV
                     ? [
-                          (customProperties) => {
-                              if (!rebuildColors) return;
+                        (customProperties) => {
+                            if (!rebuildColors) return;
 
-                              let contentJson = '{\n';
-                              let contentTs = '// @ts-ignore\n';
+                            let contentJson = '{\n';
+                            let contentTs = '// @ts-ignore\n';
 
-                              contentTs += 'export type Colors = {\n';
+                            contentTs += 'export type Colors = {\n';
 
-                              for ([c, v] of Object.entries(customProperties)) {
-                                  if (c.startsWith('--c-')) {
-                                      contentTs += `    '${c.substr(4)}': '${v}';\n`;
-                                      contentJson += `    "${c}": "${v}",\n`;
-                                  }
-                              }
+                            for ([c, v] of Object.entries(customProperties)) {
+                                if (c.startsWith('--c-')) {
+                                    contentTs += `    ${c.slice(4).includes('-') ? `'${c.slice(4)}'` : `${c.slice(4)}`}: '${v}';\n`;
+                                    contentJson += `    "${c}": "${v}",\n`;
+                                }
+                            }
 
-                              contentTs += '};\n';
-                              contentJson = contentJson.slice(0, -2) + '\n}\n';
+                            contentTs += '};\n';
+                            contentJson = contentJson.slice(0, -2) + '\n}\n';
 
-                              fs.writeFileSync(path.resolve(__dirname, 'src/common/colors.json'), contentJson);
+                            fs.writeFileSync(path.resolve(__dirname, 'src/shared/common/colors.json'), contentJson);
 
-                              fs.writeFileSync(path.resolve(__dirname, 'src/common/colors.ts'), contentTs);
-                          }
-                      ]
+                            fs.writeFileSync(path.resolve(__dirname, 'src/shared/common/colors.ts'), contentTs);
+                        }
+                    ]
                     : []
             }
             // 'postcss-hexrgba': {},
---
+++ packages/scanner/src/shared/common/colors.ts
@@ -0,0 +1,15 @@
+// @ts-ignore
+export type Colors = {
+    red: '#ff0000';
+    green: '#00ff00';
+    yellow: '#fad900';
+    dark: '#333333';
+    light: '#f5f5f5';
+    blue: '#3D2EE8';
+    white: '#ffffff';
+    grey: '#F5F7F8';
+    error: '#DD2E4E';
+    success: '#1A9A6B';
+    txt: '#1B1B1B';
+    'secondary-txt': '#8F8F8F';
+};
