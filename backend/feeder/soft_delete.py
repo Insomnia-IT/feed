7ad79e19720a0dda4f03f0eba246a9a3178c2a53
Evgenia Ovchinnikova <evgenia.ovchinnikova@orange.com>
2024-04-26 16:21:20 +0300

    soft delete пока никуда не подключен

---
+++ backend/feeder/soft_delete.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+
+from django.db import models
+from rest_framework.viewsets import ModelViewSet
+
+
+class SoftDeleteQuerySet(models.QuerySet):
+    def delete(self):
+        return super(SoftDeleteQuerySet, self).update(deleted_at=datetime.utcnow())
+
+    def restore(self):
+        return super(SoftDeleteQuerySet, self).update(deleted_at=None)
+
+    def hard_delete(self):
+        return super(SoftDeleteQuerySet, self).delete()
+
+    def alive(self):
+        return self.filter(deleted_at=None)
+
+    def deleted(self):
+        return self.exclude(deleted_at=None)
+
+
+class SoftDeleteManager(models.Manager):
+    def __init__(self, *args, **kwargs):
+        super(SoftDeleteManager, self).__init__(*args, **kwargs)
+
+    def get_queryset(self):
+        return SoftDeleteQuerySet(self.model)
+
+    def hard_delete(self):
+        return self.get_queryset().hard_delete()
+
+    def all(self):
+        return super(SoftDeleteManager, self).all()
+
+
+class SoftDeleteModelMixin:
+    deleted_at = models.DateTimeField(editable=False, null=True, default=None)
+
+    class Meta:
+        abstract = True
+
+    @property
+    def is_deleted(self):
+        return bool(self.deleted_at)
+
+    def delete(self, *args, **kwargs):
+        self.deleted_at = datetime.utcnow()
+        self.save(**kwargs)
+
+    def hard_delete(self):
+        super(SoftDeleteModelMixin, self).delete()
+
+
+class SoftDeleteViewMixin(ModelViewSet):
+    def get_queryset(self):
+        return super().get_queryset().alive()
