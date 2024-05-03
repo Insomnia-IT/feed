41c09a7098877628de13fdbcc3e9f39be119b624
unknown <kerch83@gmail.com>
2024-05-03 09:20:07 +0300

    ФИКС: закрыть возможность смены категории кастомного поля

--- packages/admin/src/components/entities/volunteer-custom-fields/common.tsx
+++ packages/admin/src/components/entities/volunteer-custom-fields/common.tsx
@@ -10,18 +10,12 @@ export const CreateEdit: FC<{ isEdit?: boolean }> = ({ isEdit }) => (
         <Form.Item label='Название' name='name' rules={Rules.required}>
             <Input />
         </Form.Item>
-        {isEdit ? (
-            <div>
-                тип данных изменить нельзя
-            </div>
-        ) : (
-            <Form.Item label='Тип данных' name='type' rules={Rules.required}>
-                <Select>
-                    <Select.Option value='string'>Строка</Select.Option>
-                    <Select.Option value='boolean'>Чекбокс</Select.Option>
-                </Select>
-            </Form.Item>
-        )}
+        <Form.Item label='Тип данных' name='type' rules={Rules.required}>
+            <Select disabled={isEdit}>
+                <Select.Option value='string'>Строка</Select.Option>
+                <Select.Option value='boolean'>Чекбокс</Select.Option>
+            </Select>
+        </Form.Item>
         <Form.Item label='Комментарий' name='comment'>
             <ReactQuill />
         </Form.Item>
