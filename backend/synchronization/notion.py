454e283c4723ba3653a5d19c3e9242a39e07e23f
ovevan89 <ovevan89@mail.ru>
2024-05-13 19:52:38 +0300

    sync add request methods

--- backend/synchronization/notion.py
+++ backend/synchronization/notion.py
@@ -1,31 +1,65 @@
-from feeder.models import Volunteer, Gender, Direction
+import logging
+
+import requests
+from django.conf import settings
+from rest_framework.exceptions import APIException
+
+from feeder.models import Volunteer
+
+logger = logging.getLogger(__name__)
 
 
 class NotionSync:
 
+    header = {"Authorization": settings.NOTION_AUTH_HEADER}
+
+    def get_request_method(self, url, params=None):
+        response = requests.get(
+            url=url,
+            params=params,
+            headers=self.header
+        )
+        if response.ok:
+            return response.json()
+        raise APIException(f'GET request for url "{url}" failed with error {response.text}')
+
+    def post_request_method(self, url, data, params=None):
+        response = requests.post(
+            url=url,
+            json=data,
+            params=params,
+            headers=self.header
+        )
+        if response.ok:
+            return True
+        raise APIException(f'POST request for url "{url}" failed with error {response.text}')
+
+    def saved_badges(self, badges):
+        delete_badges_ids = []
+        create_or_update_badges = []
+        for badge in badges:
+            if badge.get("deleted", False):
+                delete_badges_ids.append(badge.get("id"))
+                continue
+            create_or_update_badges.append(Volunteer(**badge))
+            Volunteer.objects.filter(id__in=delete_badges_ids).delete()
+            # todo пеедавать признак что из синхронизации???
+            Volunteer.objects.bulk_create(create_or_update_badges, update_conflicts=True, unique_fields=['uuid'])
+
     def sync_from_notion(self):
-        data = {
-            "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-            "name": "test create vol",
-            "first_name": "test",
-            "last_name": "create",
-            "gender": Gender.objects.get(id="MALE"),
-            # "infant": True,
-            "phone": "8 900 000 00 00",
-            "is_vegan": True,
-            "role": "ORGANIZER",
-            "position": "string",
-            "photo": "https://upload.wikimedia.org/wikipedia/commons/f/f5/Example_image.jpg",
-            # "person": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-            "comment": "string",
-            "notion_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-            # "departments": Department.objects.filter(name__in=["Сайт", "IT"])
-        }
-        volunteer = Volunteer.objects.create(**data, from_sync=True)
+        params = {"from_date": "2024-05-07T11:00:00.000Z"}
+        try:
+            data = self.get_request_method(
+                url=settings.AGREEMOD_PEOPLE_URL,
+                params=params
+            )
+        except Exception as e:
+            logger.error(e)
+            raise APIException()
 
     def sync_to_notion(self):
         pass
 
     def main(self):
-        self.sync_from_notion()
         self.sync_to_notion()
+        self.sync_from_notion()
