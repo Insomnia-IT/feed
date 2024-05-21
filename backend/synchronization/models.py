f662588370fd87f4fe0d959520f320731998372d
ovevan89 <ovevan89@mail.ru>
2024-05-21 23:11:36 +0300

    chnage sync model

--- backend/synchronization/models.py
+++ backend/synchronization/models.py
@@ -1,15 +1,25 @@
 from django.db import models
 
 
-class SynchronizationToSystemActions(models.Model):
+class SynchronizationSystemActions(models.Model):
     SYSTEM_NOTION = "notion"
     SYSTEM_CHOICES = (
         (SYSTEM_NOTION, "notion"),
     )
+    DIRECTION_TO_SYSTEM = "to_system"
+    DIRECTION_FROM_SYSTEM = "from_system"
+
+    DIRECTION_CHOICES = (
+        (DIRECTION_TO_SYSTEM, "to_system"),
+        (DIRECTION_FROM_SYSTEM, "from_system"),
+    )
 
     system = models.CharField(max_length=32, choices=SYSTEM_CHOICES)
+    direction = models.CharField(max_length=32, choices=DIRECTION_CHOICES)
     date = models.DateTimeField()
-    latest = models.BooleanField(default=True)
+    latest = models.BooleanField(default=False)
+    success = models.BooleanField(default=True)
+    error = models.CharField(max_length=512, null=True, blank=True)
 
     class Meta:
         verbose_name = "Синхронизация В"
