15f0e46cb1a1d6d1b2b9f66dd03243b4962a464f
volnyagin <vconst@users.noreply.github.com>
2024-10-27 17:10:23 +0300

    backend and nginx

--- entry_backend.sh
+++ entry_backend.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 cd /app/backend
-./manage.py migrate
-./manage.py shell < create_user.py
-./manage.py loaddata colors feed_types kitchens access_roles volunteer_roles engagement_roles transports genders statuses direction_types
-./manage.py runserver localhost:8000 &
 
 python ./cron_config.py
 
 /usr/sbin/crond -f -l 8 &
 
-nginx
+./manage.py migrate
+./manage.py shell < create_user.py
+./manage.py loaddata colors feed_types kitchens access_roles volunteer_roles engagement_roles transports genders statuses direction_types
+./manage.py runserver 0.0.0.0:8000
