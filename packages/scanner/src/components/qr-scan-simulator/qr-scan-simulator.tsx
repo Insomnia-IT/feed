04eeee0fdf366e58afca1f01014b4e40255ca039
Voodoo <killer4815@mail.ru>
2024-12-13 22:59:53 +0300

    style(ScanSimulator): добавил групповой бейдж

--- packages/scanner/src/components/qr-scan-simulator/qr-scan-simulator.tsx
+++ packages/scanner/src/components/qr-scan-simulator/qr-scan-simulator.tsx
@@ -5,14 +5,15 @@ import css from './qr-scan-simulator.module.css';
 const options = [
     { label: 'Артем Новиков', value: 'abcfd1e5' },
     { label: 'не существует', value: 'qwerty1234' },
-    { label: 'Артур', value: '960e4452d6e34747a95b076cc3c22770' }
+    { label: 'Артур', value: '960e4452d6e34747a95b076cc3c22770' },
+    { label: 'Театр', value: 'e4baa6e988b140c2be74797727be7180' }
 ];
 
 export const ScanSimulator: React.FC<{
     withSelection?: boolean;
     setScanResult: (scanResult: string) => void;
-}> = ({ setScanResult, withSelection }) => {
-    const [input, setInput] = useState('960e4452d6e34747a95b076cc3c22770');
+}> = ({ setScanResult, withSelection = false }) => {
+    const [input, setInput] = useState('e4baa6e988b140c2be74797727be7180');
 
     return (
         <div className={css.scanSimulatorBlock}>
@@ -34,8 +35,4 @@ export const ScanSimulator: React.FC<{
             </button>
         </div>
     );
-};
-
-ScanSimulator.defaultProps = {
-    withSelection: false
 };
