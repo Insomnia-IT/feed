ccefcd6357855252c18087c4758ad2fe8eb120ee
volnyagin <vconst@users.noreply.github.com>
2024-12-29 15:29:52 +0300

    Поправить сборку для admin vite

--- entry.sh
+++ entry.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-cd /app/packages/api
-rm -rf /app/packages/api/_db/*
-/app/node_modules/.bin/typeorm-seeding seed -c ./seed.js
-PORT=4262 /app/node_modules/.bin/nest start &
-
-cd /app/packages/admin
-PORT=4301 /app/node_modules/.bin/next start &
-
 cd /app/backend
 ./manage.py migrate
 ./manage.py shell < create_user.py
