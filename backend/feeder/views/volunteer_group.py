99d67a071ddcff7e9a4b178ee5b6062744a4ef4e
Petr Karasev <petr.karasev@datadvance.net>
2025-03-24 02:29:41 +0300

    Request for group operation was changes

--- backend/feeder/views/volunteer_group.py
+++ backend/feeder/views/volunteer_group.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets, permissions, filters, status
@@ -16,13 +17,13 @@ class VolunteerGroupViewSet(APIView):
 
     @extend_schema(
         request=serializers.VolunteerGroupSerializer(),
-        responses={200: serializers.SimpleResponse},
     )
     def post(self, request, *args, **kwargs):
         volunteers_ids = request.data.get('volunteers_ids', [])
-        new_data = request.data.get('new_data', {})
-        print("ids: ", volunteers_ids)
-        print('new_data: ', new_data)
+        new_data = {}
+        new_data_list = request.data.get('field_list', {})
+        for entity in new_data_list:
+            new_data[entity['field']] = entity['data']
         if not isinstance(volunteers_ids, list) or len(volunteers_ids) == 0:
             return Response({"error": "volunteer_ids should be a non-empty list"}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -35,7 +36,7 @@ class VolunteerGroupViewSet(APIView):
         # Сохраняем предыдущие значения полей
         volunteers_before_update = Volunteer.objects.filter(id__in=volunteers_ids).values()
         original_data = {volunteer['id']: volunteer for volunteer in volunteers_before_update}
-        print("Original data: ", original_data)
+        print("New data: ", new_data)
         for volunteer_id in volunteers_ids:
             try:
                 volunteer_instance = Volunteer.objects.get(id=volunteer_id)
@@ -47,38 +48,33 @@ class VolunteerGroupViewSet(APIView):
                     errors.append({"id": volunteer_id, "errors": serializer.errors})
             except Volunteer.DoesNotExist:
                 errors.append({"error": f"Volunteer with id {volunteer_id} does not exist", "volunteer_id": volunteer_id})
-
+            except Exception as e:
+                errors.append({"error": "Failed to renew volunteer data", "errors": e})
         if errors:
             return Response({"updated": updated_volunteers, "errors": errors}, status=status.HTTP_400_BAD_REQUEST)
 
         # Сохраняем новое состояние полей
-        volunteers_after_update = Volunteer.objects.filter(id__in=volunteers_ids).values()
-        new_data = {volunteer['id']: volunteer for volunteer in volunteers_after_update}
-
-        # Создаем лог
-        print("Original data1: ", original_data)
-        log_data = {
-            "volunteers_ids": volunteers_ids,
-            "original_data": original_data, #json.dumps(original_data, default=str),
-            "new_data": json.dumps(new_data, default=str)
-        }
         try:
+            volunteers_after_update = Volunteer.objects.filter(id__in=volunteers_ids).values()
+            new_data = {volunteer['id']: volunteer for volunteer in volunteers_after_update}
+            # Создаем лог
             volunteer_log_instance = VolunteerGroupOperation.objects.create(
                 volunteers_ids=volunteers_ids,
                 original_data=json.dumps(original_data, default=str),
                 new_data=json.dumps(new_data, default=str)
             )
             volunteer_log_instance.save()
-        except:
-            return Response({"error": "Failed to log the operation", "errors": log_serializer.errors},
+        except Exception as e:
+            return Response({"error": "Failed to log the operation", "errors": e},
                         status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        return Response( status=status.HTTP_200_OK)#Response(updated_volunteers, status=status.HTTP_200_OK)
+        return Response(
+            {"id": volunteer_log_instance.group_operation_id}, status=status.HTTP_200_OK)
 
 class VolunteerGroupDeleteViewSet(APIView):  # viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated,]
     @extend_schema(responses={200: serializers.SimpleResponse},)
-    def delete(self, request, pk, format=None):
+    def delete(self, request, pk):
         operation_id = pk
         if not operation_id:
             return Response({"error": "operation_id is required"}, status=status.HTTP_400_BAD_REQUEST)
@@ -94,6 +90,7 @@ class VolunteerGroupDeleteViewSet(APIView):  # viewsets.ModelViewSet):
         errors = []
 
         for volunteer_data in json.loads(original_data).values():
+            print(volunteer_data)
             volunteer_id = volunteer_data['id']
             try:
                 volunteer_instance = Volunteer.objects.get(id=volunteer_id)
