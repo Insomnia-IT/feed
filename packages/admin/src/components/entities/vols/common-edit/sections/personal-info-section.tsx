c549d66986b61ed404ea43a000af24407144ed17
yambikov <yambikov@yandex.ru>
2025-03-30 19:56:41 +0300

    04 photo

--- packages/admin/src/components/entities/vols/common-edit/sections/personal-info-section.tsx
+++ packages/admin/src/components/entities/vols/common-edit/sections/personal-info-section.tsx
@@ -1,4 +1,4 @@
-import { Form, Input, Select, Checkbox } from 'antd';
+import { Form, Input, Select, Checkbox, Image } from 'antd';
 
 import { Rules } from 'components/form';
 import HorseIcon from 'assets/icons/horse-icon';
@@ -10,160 +10,178 @@ import type { DirectionEntity, IPerson } from 'interfaces';
 import { useSelect } from '@refinedev/antd';
 
 export const PersonalInfoSection = ({
-    isBlocked,
-    denyBadgeEdit,
-    denyFeedTypeEdit,
-    feedTypeOptions,
-    kitchenOptions,
-    genderOptions,
-    canEditGroupBadge,
-    colorTypeOptions,
-    groupBadgeOptions,
-    handleQRChange,
-    person
+  isBlocked,
+  denyBadgeEdit,
+  denyFeedTypeEdit,
+  feedTypeOptions,
+  kitchenOptions,
+  genderOptions,
+  canEditGroupBadge,
+  colorTypeOptions,
+  groupBadgeOptions,
+  handleQRChange,
+  person
 }: {
-    isBlocked: boolean;
-    denyBadgeEdit: boolean;
-    denyFeedTypeEdit: boolean;
-    feedTypeOptions: { label: string; value: string | number }[];
-    kitchenOptions: { label: string; value: string | number }[];
-    genderOptions: { label: string; value: string | number }[];
-    canEditGroupBadge: boolean;
-    colorTypeOptions: { label: string; value: string | number }[];
-    groupBadgeOptions: { label: string; value: string | number }[];
-    handleQRChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    person: IPerson | null;
+  isBlocked: boolean;
+  denyBadgeEdit: boolean;
+  denyFeedTypeEdit: boolean;
+  feedTypeOptions: { label: string; value: string | number }[];
+  kitchenOptions: { label: string; value: string | number }[];
+  genderOptions: { label: string; value: string | number }[];
+  canEditGroupBadge: boolean;
+  colorTypeOptions: { label: string; value: string | number }[];
+  groupBadgeOptions: { label: string; value: string | number }[];
+  handleQRChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  person: IPerson | null;
 }) => {
-    const form = Form.useFormInstance();
+  const form = Form.useFormInstance();
 
-    const onGroupBadgeClear = () => {
-        setTimeout(() => {
-            form.setFieldValue('group_badge', '');
-        });
-    };
-
-    const mainRole = Form.useWatch('main_role', form);
-    const allowEmptyDirections = ['FELLOW', 'ART_FELLOW', 'VIP', 'PRESS', 'CONTRACTOR'].includes(mainRole);
-    const allowRoleEdit = useCanAccess({ action: 'role_edit', resource: 'volunteers' });
-    const { selectProps: directionsSelectProps } = useSelect<DirectionEntity>({
-        resource: 'directions',
-        optionLabel: 'name',
-        optionValue: 'id'
+  const onGroupBadgeClear = () => {
+    setTimeout(() => {
+      form.setFieldValue('group_badge', '');
     });
+  };
 
-    return (
-        <>
-            <div className={styles.formSection__title}>
-                Волонтер
-                {isBlocked && (
-                    <div className={styles.bannedWrap}>
-                        <span className={styles.bannedDescr}>Забанен</span>
-                    </div>
-                )}
+  const mainRole = Form.useWatch('main_role', form);
+  
+  const allowEmptyDirections = ['FELLOW', 'ART_FELLOW', 'VIP', 'PRESS', 'CONTRACTOR'].includes(mainRole);
+  const allowRoleEdit = useCanAccess({ action: 'role_edit', resource: 'volunteers' });
+  const { selectProps: directionsSelectProps } = useSelect<DirectionEntity>({
+    resource: 'directions',
+    optionLabel: 'name',
+    optionValue: 'id'
+  });
+
+  // const photo = form.getFieldValue('photo');
+  const photo = 'https://sun1-85.userapi.com/s/v1/ig2/ortGZiVTcUqsOrQYxnjLm7MGA6ZRTLMDTs57g0ObQR7Tcg7Sn58SSkLevJyPNMfK5MCpbdJV33SLQd8IgEbPJv_o.jpg?quality=95&crop=428,36,1544,1544&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720,1080x1080,1280x1280,1440x1440&ava=1&cs=400x400';
+  console.log(photo);
+
+
+  return (
+    <>
+      <div className={styles.formSection__title}>
+        Волонтер
+        {isBlocked && (
+          <div className={styles.bannedWrap}>
+            <span className={styles.bannedDescr}>Забанен</span>
+          </div>
+        )}
+      </div>
+      <div className={styles.personalWrap}>
+        <div className={styles.photoWrap}>
+
+          {photo ? (
+            <Image
+              src={photo}
+              alt="Фото волонтера"
+              width={120}
+              height={120}
+              style={{ objectFit: 'cover', borderRadius: '2px', border: '1px solid #D9D9D9' }}
+            />
+          ) : (
+            <HorseIcon />
+          )}
+          
+        </div>
+        <div className={styles.personalInfoWrap}>
+          <div className={styles.nickNameLastnameWrap}>
+            <div className={`${styles.nameInput} ${styles.padInp}`}>
+              <Form.Item label="Надпись на бейдже" name="name" rules={Rules.required}>
+                <Input readOnly={denyBadgeEdit} />
+              </Form.Item>
             </div>
-            <div className={styles.personalWrap}>
-                <div className={styles.photoWrap}>
-                    <HorseIcon />
-                </div>
-                <div className={styles.personalInfoWrap}>
-                    <div className={styles.nickNameLastnameWrap}>
-                        <div className={`${styles.nameInput} ${styles.padInp}`}>
-                            <Form.Item label="Надпись на бейдже" name="name" rules={Rules.required}>
-                                <Input readOnly={denyBadgeEdit} />
-                            </Form.Item>
-                        </div>
-                        <div className={`${styles.nameInput} ${styles.padInp}`}>
-                            <Form.Item label="Имя" name="first_name">
-                                <Input readOnly={denyBadgeEdit} />
-                            </Form.Item>
-                        </div>
-                        <div className={styles.nameInput}>
-                            <Form.Item label="Фамилия" name="last_name">
-                                <Input readOnly={denyBadgeEdit} />
-                            </Form.Item>
-                        </div>
-                        <div className={styles.phoneInput}>
-                            <Form.Item label="Telegram" name={['person', 'telegram']}>
-                                <Input />
-                            </Form.Item>
-                        </div>
-                    </div>
-                    <div className={styles.nickNameLastnameWrap}>
-                        <div className={styles.phoneInput}>
-                            <Form.Item label="Телефон" name="phone">
-                                <Input type="phone" />
-                            </Form.Item>
-                        </div>
-                        <div className={styles.genderSelect}>
-                            <Form.Item label="Пол волонтера" name="gender">
-                                <Select disabled={denyBadgeEdit} options={genderOptions} />
-                            </Form.Item>
-                        </div>
-                    </div>
-                </div>
+            <div className={`${styles.nameInput} ${styles.padInp}`}>
+              <Form.Item label="Имя" name="first_name">
+                <Input readOnly={denyBadgeEdit} />
+              </Form.Item>
             </div>
-            <div className={styles.kitchenTypeWrap}>
-                <div className={styles.kitchenSelect}>
-                    <Form.Item label="Кухня" name="kitchen" rules={Rules.required}>
-                        <Select options={kitchenOptions} />
-                    </Form.Item>
-                </div>
-                <div className={styles.typeMeal}>
-                    <Form.Item label="Тип питания" name="feed_type" rules={Rules.required}>
-                        <Select disabled={denyFeedTypeEdit} options={feedTypeOptions} />
-                    </Form.Item>
-                </div>
+            <div className={styles.nameInput}>
+              <Form.Item label="Фамилия" name="last_name">
+                <Input readOnly={denyBadgeEdit} />
+              </Form.Item>
             </div>
-            <div className={styles.isActiveCheckboxWrap}>
-                <div className={styles.isActiveCheckbox}>
-                    <Form.Item name="is_vegan" valuePropName="checked">
-                        <Checkbox>Веган</Checkbox>
-                    </Form.Item>
-                </div>
+            <div className={styles.phoneInput}>
+              <Form.Item label="Telegram" name={['person', 'telegram']}>
+                <Input />
+              </Form.Item>
             </div>
-            <div className={styles.badgeInfoWrap}>
-                <div className={styles.badgeInfo}>
-                    <Form.Item label="QR бейджа" name="qr" rules={Rules.required}>
-                        <Input disabled={denyBadgeEdit} onChange={handleQRChange} />
-                    </Form.Item>
-                </div>
-                <div className={styles.badgeInfo}>
-                    <Form.Item label="Групповой бейдж" name="group_badge">
-                        <Select
-                            disabled={!canEditGroupBadge}
-                            allowClear
-                            options={groupBadgeOptions}
-                            onClear={onGroupBadgeClear}
-                        />
-                    </Form.Item>
-                </div>
+          </div>
+          <div className={styles.nickNameLastnameWrap}>
+            <div className={styles.phoneInput}>
+              <Form.Item label="Телефон" name="phone">
+                <Input type="phone" />
+              </Form.Item>
             </div>
-            <div className={styles.badgeInfoWrap}>
-                <div className={styles.badgeInfo}>
-                    <div className={styles.badgeInfoPart}>
-                        <Form.Item label="Партия бейджа" name="printing_batch" className={styles.badgeInfoPartItem}>
-                            <Input readOnly disabled={denyBadgeEdit} />
-                        </Form.Item>
-                        <Form.Item label="Номер бейджа" name="badge_number" className={styles.badgeInfoPartItem}>
-                            <Input disabled={denyBadgeEdit} />
-                        </Form.Item>
-                    </div>
-                </div>
-                <div className={styles.badgeInfo}>
-                    <Form.Item label="Цвет бейджа" name="color_type">
-                        <Select disabled={true} options={colorTypeOptions} />
-                    </Form.Item>
-                </div>
-                <div className={styles.hrInput}>
-                    <Form.Item
-                        label="Служба / Локация"
-                        name="directions"
-                        rules={allowEmptyDirections ? undefined : Rules.required}
-                    >
-                        <Select disabled={!allowRoleEdit && !!person} mode="multiple" {...directionsSelectProps} />
-                    </Form.Item>
-                </div>
+            <div className={styles.genderSelect}>
+              <Form.Item label="Пол волонтера" name="gender">
+                <Select disabled={denyBadgeEdit} options={genderOptions} />
+              </Form.Item>
             </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+      <div className={styles.kitchenTypeWrap}>
+        <div className={styles.kitchenSelect}>
+          <Form.Item label="Кухня" name="kitchen" rules={Rules.required}>
+            <Select options={kitchenOptions} />
+          </Form.Item>
+        </div>
+        <div className={styles.typeMeal}>
+          <Form.Item label="Тип питания" name="feed_type" rules={Rules.required}>
+            <Select disabled={denyFeedTypeEdit} options={feedTypeOptions} />
+          </Form.Item>
+        </div>
+      </div>
+      <div className={styles.isActiveCheckboxWrap}>
+        <div className={styles.isActiveCheckbox}>
+          <Form.Item name="is_vegan" valuePropName="checked">
+            <Checkbox>Веган</Checkbox>
+          </Form.Item>
+        </div>
+      </div>
+      <div className={styles.badgeInfoWrap}>
+        <div className={styles.badgeInfo}>
+          <Form.Item label="QR бейджа" name="qr" rules={Rules.required}>
+            <Input disabled={denyBadgeEdit} onChange={handleQRChange} />
+          </Form.Item>
+        </div>
+        <div className={styles.badgeInfo}>
+          <Form.Item label="Групповой бейдж" name="group_badge">
+            <Select
+              disabled={!canEditGroupBadge}
+              allowClear
+              options={groupBadgeOptions}
+              onClear={onGroupBadgeClear}
+            />
+          </Form.Item>
+        </div>
+      </div>
+      <div className={styles.badgeInfoWrap}>
+        <div className={styles.badgeInfo}>
+          <div className={styles.badgeInfoPart}>
+            <Form.Item label="Партия бейджа" name="printing_batch" className={styles.badgeInfoPartItem}>
+              <Input readOnly disabled={denyBadgeEdit} />
+            </Form.Item>
+            <Form.Item label="Номер бейджа" name="badge_number" className={styles.badgeInfoPartItem}>
+              <Input disabled={denyBadgeEdit} />
+            </Form.Item>
+          </div>
+        </div>
+        <div className={styles.badgeInfo}>
+          <Form.Item label="Цвет бейджа" name="color_type">
+            <Select disabled={true} options={colorTypeOptions} />
+          </Form.Item>
+        </div>
+        <div className={styles.hrInput}>
+          <Form.Item
+            label="Служба / Локация"
+            name="directions"
+            rules={allowEmptyDirections ? undefined : Rules.required}
+          >
+            <Select disabled={!allowRoleEdit && !!person} mode="multiple" {...directionsSelectProps} />
+          </Form.Item>
+        </div>
+      </div>
+    </>
+  );
 };
