7b23101bb555f64e45e8239dce2a96cf89f7c298
yambikov <yambikov@yandex.ru>
2025-04-09 15:54:47 +0300

    18 Breadcrumb ready

--- packages/admin/src/components/entities/vols/edit.tsx
+++ packages/admin/src/components/entities/vols/edit.tsx
@@ -1,6 +1,8 @@
 import { Edit, useForm } from '@refinedev/antd';
-import { Form } from 'antd';
+import { Form, Breadcrumb } from 'antd';
 import type { IResourceComponentsProps } from '@refinedev/core';
+import { useBreadcrumb } from '@refinedev/core';
+import { Link } from 'react-router-dom';
 
 import type { VolEntity } from 'interfaces';
 
@@ -16,8 +18,34 @@ export const VolEdit: FC<IResourceComponentsProps> = () => {
     });
     const { onClick, onMutationSuccess, renderModal } = useSaveConfirm(form, saveButtonProps);
 
+    const name = Form.useWatch('name', form);
+    const volunteerName = name || 'Волонтер';
+    const { breadcrumbs } = useBreadcrumb();
+
+    const CustomBreadcrumb = () => {
+        if (!breadcrumbs) return null;
+
+        return (
+            <Breadcrumb>
+                {breadcrumbs.map((item, index) => {
+                    const isLast = index === breadcrumbs.length - 1;
+                    return (
+                        <Breadcrumb.Item key={item.label}>
+                            {isLast ? (
+                                volunteerName
+                            ) : (
+                                <Link to={item.href || '#'}>{item.label}</Link>
+                            )}
+                        </Breadcrumb.Item>
+                    );
+                })}
+            </Breadcrumb>
+        );
+    };
+
     return (
         <Edit
+            breadcrumb={<CustomBreadcrumb />}
             saveButtonProps={{
                 ...saveButtonProps,
                 onClick
