07aab73f927f6e486b095a295db84d8169b5ecf1
Voodoo <killer4815@mail.ru>
2025-05-10 12:08:49 +0300

    style(VolInfo): правки по ревью

--- packages/scanner/src/components/post-scan/post-scan-cards/vol-info/vol-info.tsx
+++ packages/scanner/src/components/post-scan/post-scan-cards/vol-info/vol-info.tsx
@@ -6,69 +6,63 @@ import { Text } from '~/shared/ui/typography';
 
 import css from './vol-info.module.css';
 
-const formatDate = (value): string => {
-    return new Date(value).toLocaleString('ru', { day: 'numeric', month: 'long' });
-};
-
 const today = dayjs();
 
 // Поиск приоритетного заезда для показа: Надо выбрать тот заезд, в который входит «сегодня». Если такого нет, выбрать ближайший (прошлый или будущий). И только в текущем году
-const findTargetArrivalDates = (arrivals: Array<Arrival>): Arrival | undefined => {
-    let targetArrival: Arrival | undefined;
-    let isInside = false;
+const calculateTargetArrivalDatesText = (arrivals: Array<Arrival>): string => {
+    let targetDates: { start: ReturnType<typeof dayjs>; end: ReturnType<typeof dayjs> } | undefined;
 
-    arrivals.forEach((arrival) => {
-        if (isInside) {
-            return;
-        }
-
+    for (const arrival of arrivals) {
         const start = dayjs(arrival.arrival_date);
         const end = dayjs(arrival.departure_date);
 
         // Другой год - пропускаем сразу
         if (!start.isSame(today, 'year')) {
-            return;
+            continue;
         }
 
         // Сегодня попадает в дату заезда/отъезда или между ними
         if (start.isSame(today, 'date') || end.isSame(today, 'date') || (start.isBefore(today) && end.isAfter(today))) {
-            targetArrival = arrival;
-            isInside = true;
+            targetDates = { start, end };
 
-            return;
+            break;
         }
 
         // Нет никакого целевого заезда - присваиваем текущий заезд.
-        if (!targetArrival) {
-            targetArrival = arrival;
-            return;
+        if (!targetDates) {
+            targetDates = { start, end };
+            continue;
         }
 
-        const targetArrivalStart = dayjs(targetArrival.arrival_date);
-        const targetArrivalEnd = dayjs(targetArrival.departure_date);
+        const targetArrivalStart = targetDates.start;
+        const targetArrivalEnd = targetDates.end;
 
         // Если дата старта текущего заезда ближе к сегодня - считаем его целевым.
         if (today.diff(targetArrivalEnd) > today.diff(start) && today.diff(targetArrivalStart) > today.diff(start)) {
-            targetArrival = arrival;
+            targetDates = { start, end };
         }
-    });
+    }
 
-    return targetArrival;
+    if (!targetDates) {
+        return '(не найдены даты заезда)';
+    }
+
+    return `(${targetDates.start.format('DD MMMM')} - ${targetDates.end.format('DD MMMM')})`;
 };
 
 export const VolInfo: FC<{
     vol: Volunteer;
 }> = ({ vol }) => {
-    const targetArrival = findTargetArrivalDates(vol.arrivals);
-    const arrivalDatesText = targetArrival
-        ? [targetArrival.arrival_date, targetArrival.departure_date].map(formatDate).join(' - ')
-        : undefined;
+    const { arrivals, directions, first_name, name } = vol;
+
+    const feedTypeText = vol.is_vegan ? 'Веган🥦' : 'Мясоед🥩';
+    const directionsText = directions.map((direction) => direction.name).join(', ');
+    const arrivalDatesText = calculateTargetArrivalDatesText(arrivals);
 
     return (
         <div className={css.volInfo}>
             <Text className={css.text}>
-                {vol.first_name} ({vol.name}), {vol.is_vegan ? 'Веган🥦' : 'Мясоед🥩'},{' '}
-                {vol.directions.map((direction) => direction.name).join(', ')} {arrivalDatesText}
+                {first_name} ({name}), {feedTypeText}, {directionsText} {arrivalDatesText}
             </Text>
         </div>
     );
