28441768570e8ae4e4c5d59c02dc49b002d8f8c9
volnyagin <vconst@users.noreply.github.com>
2025-05-17 20:16:34 +0300

    Add WashesListSerializer to fetch volunteer and actor details

--- backend/feeder/views/wash.py
+++ backend/feeder/views/wash.py
@@ -1,8 +1,9 @@
+from feeder.views.mixins import MultiSerializerViewSetMixin
 from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from feeder.models import Wash
-from feeder.serializers import WashSerializer
+from feeder.serializers import WashSerializer, WashListSerializer
 from django_filters import rest_framework as django_filters
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -14,10 +15,13 @@ class WashFilter(django_filters.FilterSet):
         model = Wash
         fields = ["volunteer", "actor"]
 
-class WashViewSet(viewsets.ModelViewSet):
+class WashViewSet(MultiSerializerViewSetMixin, viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Wash.objects.all()
     serializer_class = WashSerializer
+    serializer_action_classes = {
+        'list': WashListSerializer
+    }
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     ordering_fields = ['created_at']
     ordering = ['-created_at'] 
