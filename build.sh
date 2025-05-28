03b29fc206acaf2c1b1cf3e868ff15f5b8a10e03
volnyagin <vconst@users.noreply.github.com>
2025-05-28 22:39:35 +0300

    force-recreate to update SKIP_BACK_SYNC and comment OTEL_ENDPOINT

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ docker compose build \
     --build-arg NEW_API_URL=http://localhost:8888/feedapi/v1 \
     --build-arg SYNC_URL=https://agreemod-dev.insomniafest.ru/api/v1/feeder/
 
-DB_DIR=./feeddb SKIP_BACK_SYNC=True PORT=8888 docker compose up -d --remove-orphans
+DB_DIR=./feeddb SKIP_BACK_SYNC=True PORT=8888 docker compose up --remove-orphans
