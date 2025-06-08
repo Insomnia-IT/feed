143544c7cf5424970f2a09af2e1411d538b77a09
Voodoo <killer4815@mail.ru>
2025-06-08 23:43:11 +0300

    feat(MassEdit): отображение заездов для выбранных волонтеров

--- packages/admin/src/components/entities/vols/vol-list/mass-edit/selected-volunteer-list/selected-volunteer-list.tsx
+++ packages/admin/src/components/entities/vols/vol-list/mass-edit/selected-volunteer-list/selected-volunteer-list.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import type { VolEntity } from 'interfaces';
+import type { ArrivalEntity, VolEntity } from 'interfaces';
 import { CloseOutlined } from '@ant-design/icons';
 import styles from './selected-volunteer-list.module.css';
 import { Button } from 'antd';
+import dayjs from 'dayjs';
+
+function findTargetArrival(vol: VolEntity): ArrivalEntity | undefined {
+    const arrivals = vol?.arrivals;
+
+    if (!arrivals) {
+        return undefined;
+    }
+
+    return arrivals.find((item) => {
+        return dayjs(item.departure_date).endOf('day').add(7, 'hours').isAfter(dayjs().startOf('day'));
+    });
+}
 
 export const SelectedVolunteerList: React.FC<{
     selectedVolunteers: VolEntity[];
@@ -14,14 +27,18 @@ export const SelectedVolunteerList: React.FC<{
         // На случай, когда нет ни ФИО, ни прозвища. Такого не должно быть, но есть(
         const fallback = 'user id #' + String(vol.id);
 
+        const currentArrival = findTargetArrival(vol);
+
         // Показываем фио, прозвище или fallback.
         return (
             <div key={vol.id} className={styles.item}>
                 <span className={styles.bold}>{title?.trim() || fallback}</span>
+                <span className={styles.arrival}>
+                    {currentArrival
+                        ? `${dayjs(currentArrival.arrival_date).format('DD.MM')} - ${dayjs(currentArrival.departure_date).format('DD.MM')}`
+                        : ''}
+                </span>
                 <Button
-                    style={{
-                        marginLeft: 'auto'
-                    }}
                     title="Убрать из выбора"
                     type="text"
                     size="small"
