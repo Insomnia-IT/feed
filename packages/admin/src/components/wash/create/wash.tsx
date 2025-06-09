cca0a3ed5fd942a4b646b0cfda38e397a57fa49d
Voodoo <killer4815@mail.ru>
2025-06-09 22:30:14 +0300

    feat(Wash): показываем кнопку перед сканом в мобильном виде

--- packages/admin/src/components/wash/create/wash.tsx
+++ packages/admin/src/components/wash/create/wash.tsx
@@ -1,12 +1,18 @@
-import { Divider } from 'antd';
+import { Button, Divider } from 'antd';
 import { FC, useCallback, useState } from 'react';
 
 import { QrScannerComponent } from 'components/qr-scanner-component';
 import { PostScan } from '../components/post-scan';
 import { useScannerController } from 'components/qr-scanner-component/hooks/useScannerController';
+import { useIsMobile } from 'shared/hooks';
+
+// Если не обновлять страницу - должно показать кнопку только один раз
+let showScanner = false;
 
 export const Wash: FC = () => {
+    const { isMobile } = useIsMobile();
     const [scannedVolunteerQr, setScannedVolunteerQr] = useState<string | undefined>();
+    const [showScannerInner, setShowScannerInner] = useState(showScanner);
 
     const scannerController = useScannerController({
         onScan: async (qr: string, { disableScan }) => {
@@ -19,6 +25,22 @@ export const Wash: FC = () => {
         setScannedVolunteerQr(undefined);
         scannerController.enableScan();
     }, [scannerController]);
+
+    if (isMobile && !showScannerInner) {
+        return (
+            <div style={{ display: 'flex', height: '50vh', justifyContent: 'center' }}>
+                <Button
+                    style={{ marginTop: 'auto' }}
+                    onClick={() => {
+                        setShowScannerInner(true);
+                        showScanner = true;
+                    }}
+                >
+                    Нажми для скана
+                </Button>
+            </div>
+        );
+    }
 
     return (
         <>
