be0a030961639e3da394cf07ffb7e5d559d4550a
volnyagin <vconst@users.noreply.github.com>
2025-06-18 19:19:33 +0300

    transaction_mode IMMEDIATE

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -228,6 +228,9 @@ DATABASES = {
         'PASSWORD': os.environ.get("DB_PASSWORD", ""),
         'HOST': os.environ.get("DB_HOST", ""),
         'PORT': os.environ.get("DB_PORT", ""),
+        'OPTIONS': {
+            'transaction_mode': 'IMMEDIATE'
+        },
     },
 }
 
