fdf3d6be0decffd058abbb58b46efef81c65fdc3
volnyagin <vconst@users.noreply.github.com>
2025-06-18 19:26:24 +0300

    call volunteer-photo-sync after notion-sync

--- backend/cron_tasks/auto_sync.py
+++ backend/cron_tasks/auto_sync.py
@@ -3,3 +3,5 @@ import requests
 
 if __name__ == "__main__":
     requests.post("http://localhost:8000/feedapi/v1/notion-sync")
+    requests.post("http://localhost:8000/feedapi/v1/volunteer-photo-sync")
+
