f0261de7df7dff5a6f41b2af0ab1086bd59f9195
volnyagin <vconst@users.noreply.github.com>
2025-07-16 18:05:58 +0300

    Добавить смену qr кода через камеру для мобильной админки

---
+++ packages/admin/src/components/entities/vols/common-edit/sections/qr-scanner-modal.tsx
@@ -0,0 +1,47 @@
+import { Form, Modal } from 'antd';
+
+import { useState } from 'react';
+import { QrScannerComponent, useScannerController } from 'components/qr-scanner-component';
+
+export const QRScannerModal = ({
+    open,
+    onClose,
+    handleQRChange
+}: {
+    open: boolean;
+    onClose: () => void;
+    handleQRChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}) => {
+    const [isModalOpened, setIsModalOpened] = useState(false);
+
+    const form = Form.useFormInstance();
+
+    const closeModal = () => {
+        setIsModalOpened(false);
+        onClose();
+    };
+
+    const scannerController = useScannerController({
+        onScan: async (qr: string) => {
+            if (qr) {
+                form.setFieldValue('qr', qr.replace(/[^A-Za-z0-9]/g, ''));
+                closeModal();
+                handleQRChange({ target: { value: qr } } as React.ChangeEvent<HTMLInputElement>);
+            }
+        }
+    });
+
+    return (
+        open && (
+            <Modal
+                title="Отсканируй новый бейдж"
+                open
+                footer={null}
+                onCancel={closeModal}
+                afterOpenChange={setIsModalOpened}
+            >
+                {isModalOpened && <QrScannerComponent scannerController={scannerController} />}
+            </Modal>
+        )
+    );
+};
