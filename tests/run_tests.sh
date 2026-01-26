e1b2216297ec81a257250b0023a19083af13a54e
volnyagin <vconst@users.noreply.github.com>
2026-01-26 22:54:07 +0300

    fix remove-orphans

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,7 +7,7 @@ echo "Gonna run regress-tests in docker compose"
 
 # docker compose run --remove-orphans easy_test |tee /tmp/tests.log
 export PORT=11832
-docker compose run tests --remove-orphans | tee /tmp/tests.log
+docker compose run --remove-orphans tests | tee /tmp/tests.log
 exitcode=${PIPESTATUS[0]}
 
 rm -rf /tmp/tests.short.log
