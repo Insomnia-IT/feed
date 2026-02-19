8092519d161aac4bcaa66be8d3ef3da72dab8f89
Voodoo <killer4815@mail.ru>
2026-02-20 00:07:53 +0300

    feat(AdaptiveDatePicker): базовый компонент выбора даты на обычном и мобильном устройстве

--- packages/admin/src/components/controls/adaptiveDatePicker/adaptive-date-picker.tsx
+++ packages/admin/src/components/controls/adaptiveDatePicker/adaptive-date-picker.tsx
@@ -1,39 +1,115 @@
 import { useScreen } from '../../../shared/providers';
-import { DatePicker, Input } from 'antd';
+import { DatePicker, Input, Row, Switch, Typography } from 'antd';
 import { CalendarPicker } from 'antd-mobile';
-import React, { useState } from 'react';
-import dayjs, { Dayjs } from 'dayjs';
+import React, { useMemo, useState } from 'react';
+import dayjs from 'dayjs';
 import { CalendarOutlined } from '@ant-design/icons';
+
+// TODO: RANGE WORKING CORRECT
 
 const AdaptiveDatePicker = ({
     value,
-    style,
     onChange,
-    open,
-    onOpenChange,
-    panelRender
+    variant
 }: {
-    open?: boolean;
-    onOpenChange?: (value: boolean) => void;
-    panelRender?: (originPanel: React.ReactNode) => React.ReactNode;
-    value?: Dayjs | null;
-    onChange: ((date: Dayjs | null, dateString: string | string[]) => void) | undefined;
-    style?: React.CSSProperties;
+    /** Показывать чек-бокс выбора периода */
+    showModeChanger?: boolean;
+    variant?: 'single' | 'range';
+    value?: string | null;
+    onChange: ((dateString?: string | null) => void) | undefined;
 }) => {
     const { isMobile = true } = useScreen();
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [_beforeString, afterString] = useMemo(() => (value ?? '')?.split(SEPARATOR) ?? [], [value]);
+
+    const [useRange, setUseRange] = useState(variant ? variant === 'range' : !!afterString);
+
     if (isMobile) {
-        return <MobileDatePicker value={value} onChange={onChange} />;
+        return <MobileDatePicker useRange={useRange} setUseRange={setUseRange} value={value} onChange={onChange} />;
+    }
+
+    return <DesktopDatePicker useRange={useRange} setUseRange={setUseRange} value={value} onChange={onChange} />;
+};
+
+const SEPARATOR = ':';
+
+const DesktopDatePicker = ({
+    value,
+    onChange,
+    useRange,
+    setUseRange
+}: {
+    open?: boolean;
+    /** Показывать чек-бокс выбора периода */
+    showModeChanger?: boolean;
+    useRange: boolean;
+    setUseRange: (value: boolean) => void;
+    value?: string | null;
+    onChange: ((dateString?: string) => void) | undefined;
+}) => {
+    const [isCalPopOpen, setIsCalPopOpen] = useState<boolean | undefined>(undefined);
+    const [beforeString, afterString] = useMemo(() => (value ?? '')?.split(SEPARATOR) ?? [], [value]);
+
+    const panelStyle = {
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: '8px',
+        padding: '8px',
+        width: useRange ? '50%' : undefined
+    };
+
+    const panelRender = (panel: React.ReactNode) => (
+        <>
+            <Row style={panelStyle}>
+                <Typography.Text>Искать в диапазоне дат</Typography.Text>
+
+                <Switch
+                    size={'small'}
+                    value={useRange}
+                    onChange={() => {
+                        setUseRange(!useRange);
+                    }}
+                />
+            </Row>
+            {panel}
+        </>
+    );
+
+    if (useRange) {
+        return (
+            <DatePicker.RangePicker
+                open={isCalPopOpen}
+                onOpenChange={(value) => setIsCalPopOpen(value)}
+                placeholder={['пусто', 'пусто']}
+                panelRender={panelRender}
+                format={{ format: 'DD.MM.YYYY' }}
+                allowEmpty={[true, true]}
+                style={{ width: 300, display: useRange ? undefined : 'none' }}
+                value={[beforeString ? dayjs(beforeString) : undefined, afterString ? dayjs(afterString) : undefined]}
+                onChange={(value) => {
+                    // Сохраняем значение в формате YYYY-MM-DD:YYYY-MM-DD
+                    const periodString = (value ?? [])
+                        .filter((e) => !!e)
+                        .map((date) => date?.format('YYYY-MM-DD'))
+                        .join(SEPARATOR);
+
+                    if (onChange) {
+                        onChange(periodString);
+                    }
+                }}
+            />
+        );
     }
 
     return (
         <DatePicker
-            onOpenChange={onOpenChange}
+            format={{ format: 'DD.MM.YYYY' }}
+            onOpenChange={(value) => setIsCalPopOpen(value)}
             panelRender={panelRender}
-            style={style}
-            value={value}
-            open={open}
-            onChange={onChange}
+            value={beforeString ? dayjs(beforeString) : undefined}
+            open={isCalPopOpen}
+            onChange={(value) => (onChange ? onChange(value?.format('YYYY-MM-DD')) : undefined)}
         />
     );
 };
@@ -41,20 +117,35 @@ const AdaptiveDatePicker = ({
 const MobileDatePicker = ({
     value,
     inputStyle,
-    onChange
+    onChange,
+    useRange,
+    setUseRange
 }: {
-    value?: Dayjs | null;
+    value?: string | null;
     inputStyle?: React.CSSProperties;
-    onChange?: (date: Dayjs | null, dateString: string | string[]) => void;
+    onChange?: (date?: string | null) => void;
+    useRange: boolean;
+    setUseRange: (value: boolean) => void;
 }) => {
     const [isPopupOpen, setIsPopupOpen] = useState(false);
-    const [tempDate, setTempDate] = useState<Date | null>(value ? value.toDate() : null);
 
-    const onChangeInner = (date: Date | null) => {
-        const dateJS = dayjs(date);
+    const [beforeString, afterString] = useMemo(() => (value ?? '')?.split(SEPARATOR) ?? [], [value]);
 
+    const [tempDate, setTempDate] = useState<Date | [Date, Date] | null>(
+        useRange ? [dayjs(beforeString)?.toDate(), dayjs(afterString)?.toDate()] : dayjs(beforeString)?.toDate()
+    );
+
+    const onChangeInner = (value: Date | [Date, Date] | null) => {
         if (onChange) {
-            onChange(dateJS, dateJS.toISOString());
+            if (Array.isArray(value)) {
+                onChange(value.map((valuePart) => dayjs(valuePart)?.format('YYYY-MM-DD')).join(SEPARATOR));
+
+                return;
+            }
+
+            const dateJS = dayjs(value);
+
+            onChange(dateJS?.format('YYYY-MM-DD'));
         }
     };
 
@@ -62,26 +153,48 @@ const MobileDatePicker = ({
         <>
             <Input
                 placeholder="Выберите дату"
-                value={value?.format('DD.MM.YYYY')}
+                value={
+                    Array.isArray(tempDate)
+                        ? tempDate.map((date) => dayjs(date).format('DD.MM.YYYY')).join(' - ')
+                        : dayjs(tempDate)?.format('DD.MM.YYYY')
+                }
                 style={inputStyle}
                 onClick={() => setIsPopupOpen(true)}
                 onClear={() => {
                     if (onChange) {
-                        onChange(null, []);
+                        onChange(null);
                     }
                 }}
                 allowClear
                 suffix={value ? null : <CalendarOutlined />}
             />
+            {/* @ts-expect-error: сложный вывод типа */}
             <CalendarPicker
                 weekStartsOn="Monday"
-                title={'Выбор даты'}
-                selectionMode="single"
+                title={
+                    <div>
+                        <span>Выбор даты</span>
+                        <div>
+                            Выбрать период{' '}
+                            <Switch
+                                size={'small'}
+                                value={useRange}
+                                onChange={() => {
+                                    setUseRange(!useRange);
+                                }}
+                            />
+                        </div>
+                    </div>
+                }
+                selectionMode={useRange ? 'range' : 'single'}
                 visible={isPopupOpen}
                 value={tempDate}
                 onClose={() => setIsPopupOpen(false)}
                 onChange={setTempDate}
                 onConfirm={onChangeInner}
+                defaultValue={tempDate}
+                min={dayjs(beforeString).subtract(1, 'year').toDate()}
+                allowClear
             />
         </>
     );
