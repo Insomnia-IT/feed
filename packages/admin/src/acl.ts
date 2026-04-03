78307b5b7f99f1a4ca708f718ab2e534dd831327
Mishok21 <mm21112000@gmail.com>
2026-04-03 13:47:33 +0300

    Fixed access to editing the badge edit

--- packages/admin/src/acl.ts
+++ packages/admin/src/acl.ts
@@ -51,8 +51,9 @@ type Action =
 
 const checkCustomPermission = (role: AppRoles, action: Action): boolean => {
     switch (action) {
-        case 'full_list':
         case 'badge_edit':
+            return role === AppRoles.ADMIN;
+        case 'full_list':
         case 'bulk_edit': // массовые изменения
             return role !== AppRoles.DIRECTION_HEAD;
         case 'status_started_assign':
