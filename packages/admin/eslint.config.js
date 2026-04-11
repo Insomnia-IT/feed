8da3f14a612dd64958a3bcd77fcfd71be3b57e3a
volnyagin <vconst@users.noreply.github.com>
2026-04-11 16:47:50 +0300

    Add no-console rule

--- packages/admin/eslint.config.js
+++ packages/admin/eslint.config.js
@@ -1,26 +1,26 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
-import { reactRefresh } from 'eslint-plugin-react-refresh';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import { defineConfig } from 'eslint/config';
 
-export default defineConfig([
-    globalIgnores(['**/dist', '**/dev-dist', '**/build', '**/.nx', '**/.turbo', '**/node_modules']),
+export default defineConfig(
+    { ignores: ['dist'] },
     {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
         files: ['**/*.{ts,tsx}'],
-        extends: [
-            js.configs.recommended,
-            tseslint.configs.recommended,
-            reactHooks.configs.flat.recommended,
-            reactRefresh.configs.vite()
-        ],
         languageOptions: {
-            ecmaVersion: 2023,
+            ecmaVersion: 2020,
             globals: globals.browser
         },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh
+        },
         rules: {
-            'no-tabs': 'error',
+            ...reactHooks.configs.recommended.rules,
+            'no-tabs': ['error'],
             'no-console': ['error', { allow: ['error'] }],
             quotes: ['error', 'single'],
             'jsx-quotes': ['error', 'prefer-double'],
@@ -29,4 +29,4 @@ export default defineConfig([
             'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
         }
     }
-]);
+);
