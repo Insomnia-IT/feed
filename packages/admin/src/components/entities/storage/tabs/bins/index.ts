1d740e3c73dabaf2676fe1c6ebab7c74799668a9
volnyagin <vconst@users.noreply.github.com>
2026-04-19 16:13:07 +0300

    fix

--- packages/admin/src/components/entities/storage/tabs/bins/index.ts
+++ packages/admin/src/components/entities/storage/tabs/bins/index.ts
@@ -1,3 +1,2 @@
 export { BinsTab } from './bins-tab';
-export { CreateBinModal } from './create-bin-modal';
 export { useBinsTab, useBinOptions } from './hooks/use-bins-tab';
--- packages/admin/src/components/entities/storage/tabs/index.ts
+++ packages/admin/src/components/entities/storage/tabs/index.ts
@@ -1,5 +1,5 @@
 export { PositionsTab, ReceiveModal, IssueModal, CreatePositionModal } from './positions';
-export { BinsTab, CreateBinModal } from './bins';
+export { BinsTab } from './bins';
 export { ItemsTab, CreateItemModal, EditItemModal } from './items';
 export { ReceivingsTab } from './receivings';
 export { IssuancesTab } from './issuances';
