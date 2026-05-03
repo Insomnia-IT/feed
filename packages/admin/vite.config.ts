c44982a7faa8ed7b0b862719582b8cd706da0ae5
Никита Максимов <john.maks595@gmail.com>
2026-05-03 11:06:04 +0300

    Более быстрый список волонтеров на мобилке

--- packages/admin/vite.config.ts
+++ packages/admin/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
         // visualizer({ open: true }), // Uncomment to visualize bundle size
         VitePWA({
             registerType: 'autoUpdate',
+            injectRegister: 'inline',
             includeAssets: ['favicon.ico', 'favicon-96x96.png', 'apple-touch-icon.png'],
             manifest: {
                 name: 'Admin Front',
