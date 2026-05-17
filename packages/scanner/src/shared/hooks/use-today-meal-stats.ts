4f34d339ab9e6e2040ed86a63e28f0d49536be34
Voodoo <killer4815@mail.ru>
2026-05-17 15:48:07 +0300

    feat(useTodayMealStats): расчет ГБ

--- packages/scanner/src/shared/hooks/use-today-meal-stats.ts
+++ packages/scanner/src/shared/hooks/use-today-meal-stats.ts
@@ -9,21 +9,34 @@ import { getToday } from 'shared/lib/date';
 export const useTodayMealStats = () => {
     const { lastSyncStart, mealTime } = useApp();
 
-    const volsOnField = useLiveQuery(
-        async () => (await getVolsOnField(getToday())).length,
-        [mealTime, lastSyncStart],
-        0
-    );
+    const volsOnField = useLiveQuery(async () => await getVolsOnField(getToday()), [mealTime, lastSyncStart], []);
 
     const todayTxs = useLiveQuery(async () => getTodayTrans(), [mealTime, lastSyncStart], []) as Array<Transaction>;
 
-    const volsFedAmount = useMemo(
+    const individualOnFieldCount = useMemo(
+        () => volsOnField.filter((v) => v.group_badge == null).length,
+        [volsOnField]
+    );
+
+    const groupVolunteersCount = useMemo(() => volsOnField.filter((v) => v.group_badge != null).length, [volsOnField]);
+
+    const individualFedCount = useMemo(
         () =>
             todayTxs.reduce((acc, curr) => {
-                if (curr.mealTime === mealTime) {
+                if (curr.mealTime === mealTime && curr.group_badge == null) {
                     return acc + curr.amount;
                 }
+                return acc;
+            }, 0),
+        [mealTime, todayTxs]
+    );
 
+    const groupFedCount = useMemo(
+        () =>
+            todayTxs.reduce((acc, curr) => {
+                if (curr.mealTime === mealTime && curr.group_badge != null) {
+                    return acc + curr.amount;
+                }
                 return acc;
             }, 0),
         [mealTime, todayTxs]
@@ -32,7 +45,9 @@ export const useTodayMealStats = () => {
     return {
         lastSyncStart,
         volsOnField,
-        volsFedAmount,
-        volsLeftAmount: Math.max(volsOnField - volsFedAmount, 0)
+        individualFedCount,
+        individualLeftCount: Math.max(individualOnFieldCount - individualFedCount, 0),
+        groupFedCount,
+        groupLeftCount: Math.max(groupVolunteersCount - groupFedCount, 0)
     };
 };
