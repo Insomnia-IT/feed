adab65f1da5d236c229018fc6c205ac0e5db640d
Nikita Maximov <john.maks595@gmail.com>
2026-06-05 16:16:37 +0300

    Удален неиспользуемый функционал на фронте

--- packages/admin/src/components/entities/vols/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './list';
-export * from './create';
-export * from './edit';
-export * from './show';
-export { useFilters } from 'components/entities/vols/vol-list/filters/use-filters';
--- packages/admin/src/components/wash/index.ts
+++
@@ -1,5 +0,0 @@
-export { Wash } from './create/wash';
-export { useAddWash } from './hooks/useAddWash';
-export { useSearchVolunteer } from './hooks/useSearchVolunteer';
-export { useVolunteerWashes } from './hooks/useVolunteerWashes';
-export * from './types';
