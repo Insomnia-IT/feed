e9adec96c1715bbe90329f59ceca659e7e6c2a69
Nikita Maximov <john.maks595@gmail.com>
2026-06-23 00:28:42 +0500

    Тултипы для инвентаря

--- packages/admin/src/components/entities/storage/tabs/items/create-item-modal.tsx
+++ packages/admin/src/components/entities/storage/tabs/items/create-item-modal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Modal, Form, Input, Checkbox, Space } from 'antd';
+import { Modal, Form, Input, Checkbox, Space, Tooltip } from 'antd';
 import type { ModalProps } from 'antd';
+import { ANONYMOUS_ITEM_TOOLTIP, UNIQUE_ITEM_TOOLTIP } from './item-tooltips';
 
 interface CreateItemModalProps {
     storageId: number;
@@ -19,12 +20,20 @@ export const CreateItemModal: React.FC<CreateItemModalProps> = ({ storageId, mod
                     <Input />
                 </Form.Item>
                 <Space size="large">
-                    <Form.Item name="is_unique" valuePropName="checked">
-                        <Checkbox>Уникальный</Checkbox>
-                    </Form.Item>
-                    <Form.Item name="is_anonymous" valuePropName="checked">
-                        <Checkbox>Анонимный</Checkbox>
-                    </Form.Item>
+                    <Tooltip title={UNIQUE_ITEM_TOOLTIP}>
+                        <span>
+                            <Form.Item name="is_unique" valuePropName="checked">
+                                <Checkbox>Уникальный</Checkbox>
+                            </Form.Item>
+                        </span>
+                    </Tooltip>
+                    <Tooltip title={ANONYMOUS_ITEM_TOOLTIP}>
+                        <span>
+                            <Form.Item name="is_anonymous" valuePropName="checked">
+                                <Checkbox>Анонимный</Checkbox>
+                            </Form.Item>
+                        </span>
+                    </Tooltip>
                 </Space>
                 <Form.Item name="storage" hidden initialValue={storageId}>
                     <Input />
--- packages/admin/src/components/entities/storage/tabs/items/edit-item-modal.tsx
+++ packages/admin/src/components/entities/storage/tabs/items/edit-item-modal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Modal, Form, Input, Checkbox, Space } from 'antd';
+import { Modal, Form, Input, Checkbox, Space, Tooltip } from 'antd';
 import type { ModalProps } from 'antd';
+import { ANONYMOUS_ITEM_TOOLTIP, UNIQUE_ITEM_TOOLTIP } from './item-tooltips';
 
 interface EditItemModalProps {
     modalProps: ModalProps;
@@ -18,12 +19,20 @@ export const EditItemModal: React.FC<EditItemModalProps> = ({ modalProps, formPr
                     <Input />
                 </Form.Item>
                 <Space size="large">
-                    <Form.Item name="is_unique" valuePropName="checked">
-                        <Checkbox>Уникальный</Checkbox>
-                    </Form.Item>
-                    <Form.Item name="is_anonymous" valuePropName="checked">
-                        <Checkbox>Анонимный</Checkbox>
-                    </Form.Item>
+                    <Tooltip title={UNIQUE_ITEM_TOOLTIP}>
+                        <span>
+                            <Form.Item name="is_unique" valuePropName="checked">
+                                <Checkbox>Уникальный</Checkbox>
+                            </Form.Item>
+                        </span>
+                    </Tooltip>
+                    <Tooltip title={ANONYMOUS_ITEM_TOOLTIP}>
+                        <span>
+                            <Form.Item name="is_anonymous" valuePropName="checked">
+                                <Checkbox>Анонимный</Checkbox>
+                            </Form.Item>
+                        </span>
+                    </Tooltip>
                 </Space>
             </Form>
         </Modal>
