4971935b51627e98e3cd397c6de22375ff925689
volnyagin <vconst@users.noreply.github.com>
2026-07-01 22:35:38 +0300

    Починка синхронизации после удаления бейджа и создания нового с таким же qr

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -68,6 +68,13 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3001',
     'http://127.0.0.1:3002',
 ]
+
+CORS_ALLOW_HEADERS = [
+    'authorization',
+    'app-version',
+    'content-type',
+]
+
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'config.urls'
